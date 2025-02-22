d7969ec05ab1d594166df9f6e5cb59dd0a866c62
Luis Minaya <luis.minaya@gmail.com>
2025-02-22 13:46:24 -0500

    First commit. Completed Part 1

---
+++ melons.py
@@ -0,0 +1,104 @@
+"""Classes for melon orders."""
+
+
+class AbstractMelonOrder:
+    """An abstract base class that other Melon Orders inherit from"""
+
+    # Attributes that will change in subclasses
+    order_type = None
+    tax = 0
+
+    def __init__(self, species, qty, country_code="US"):
+        """Initialize melon order attributes"""
+
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.country_code = country_code.upper()
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total
+    
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
+
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+    ## Question, why do we not need self for these attributes?
+    order_type = "domestic"
+    tax = 0.08
+
+
+    # def __init__(self, species, qty):
+    #     """Initialize melon order attributes."""
+
+    #     self.species = species
+    #     self.qty = qty
+    #     self.shipped = False
+    #     self.order_type = "domestic"
+    #     self.tax = 0.08
+
+    # def get_total(self):
+    #     """Calculate price, including tax."""
+
+    #     base_price = 5
+    #     total = (1 + self.tax) * self.qty * base_price
+
+    #     return total
+
+    # def mark_shipped(self):
+    #     """Record the fact than an order has been shipped."""
+
+    #     self.shipped = True
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    order_type = "international"
+    tax = 0.17
+
+
+
+    # def __init__(self, species, qty, country_code):
+    #     """Initialize melon order attributes."""
+
+    #     self.species = species
+    #     self.qty = qty
+    #     self.country_code = country_code
+    #     self.shipped = False
+    #     self.order_type = "international"
+    #     self.tax = 0.17
+
+    # def get_total(self):
+    #     """Calculate price, including tax."""
+
+    #     base_price = 5
+    #     total = (1 + self.tax) * self.qty * base_price
+
+    #     return total
+
+    # def mark_shipped(self):
+    #     """Record the fact than an order has been shipped."""
+
+    #     self.shipped = True
+
+    # def get_country_code(self):
+    #     """Return the country code."""
+
+    #     return self.country_code
